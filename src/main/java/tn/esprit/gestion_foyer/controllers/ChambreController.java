caac011c44dec004b93c9269fc0d89ce6258c4df
Mohamed Amine Larbi <larbi.mohamedamine@esprit.tn>
2025-10-18 16:27:52 +0100

    Controller chambre is done

---
+++ src/main/java/tn/esprit/gestion_foyer/controllers/ChambreController.java
@@ -0,0 +1,39 @@
+package tn.esprit.gestion_foyer.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.gestion_foyer.entities.Chambre;
+import tn.esprit.gestion_foyer.services.IChambreService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/chambreController")
+@AllArgsConstructor
+public class ChambreController {
+    final IChambreService chambreService;
+    @PostMapping("/addChambre")
+    Chambre addChambre(@RequestBody Chambre chambre){
+        return chambreService.addOrUpdateChambre(chambre);
+    }
+
+    @PutMapping("/updateChambre")
+    Chambre updateChambre(@RequestBody Chambre chambre){
+        return chambreService.addOrUpdateChambre(chambre);
+    }
+
+    @DeleteMapping
+    void deleteChambre(@RequestBody long  idChambre){
+        chambreService.deleteChambre(idChambre);
+    }
+
+    @GetMapping("/findAllChambre")
+    List<Chambre> findAllChambre(){
+        return chambreService.findAll();
+    }
+
+    @GetMapping("/findChambre")
+    Chambre findChambreById(@RequestParam long idChambre){
+        return chambreService.findChambreById(idChambre);
+    }
+}
